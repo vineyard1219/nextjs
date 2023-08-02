dab9b12f3c61b0a8caed46a162e92289fd882900
vineyard1219 <skrud1219@naver.com>
2023-08-02 22:06:36 +0900

    json 파일 가져오기

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -2,58 +2,43 @@ import React from 'react'
 import styles from './page.module.css'
 import Button from '@/components/Button/Button'
 import Image from 'next/image'
+import { items } from './data'
+import { notFound } from 'next/navigation'
+
+const getData = (cat) => {
+  const data = items[cat]
+
+  if(data){
+    return data
+  }
+
+  return notFound()
+}
 
 // params는 라우터 연결 컴포넌트에 데이터 전달하는데 사용
 const Category = ({params}) => {
-  console.log(params)
+  const data = getData(params.category)
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text='See More' url='#'/>
+      
+      {data.map((item) =>(
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text='See More' url='#'/>
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              fill={true}
+              src={item.image}
+              alt=''
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image
-          className={styles.img}
-            fill={true}
-            src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg'
-            alt=''
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text='See More' url='#'/>
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-          className={styles.img}
-            fill={true}
-            src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg'
-            alt=''
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text='See More' url='#'/>
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-          className={styles.img}
-            fill={true}
-            src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg'
-            alt=''
-          />
-        </div>
-      </div>
+      ))}
     </div>
   ) 
 }
